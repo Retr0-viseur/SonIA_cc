4724624023cf1868476f78a052652bfc4ea83375
Retr0-viseur <remi.mergen@gmail.com>
2020-12-02 19:49:42 +0100

    test inclusion vel

--- sonIA/src/sonIA.py
+++ sonIA/src/sonIA.py
@@ -4,5 +4,9 @@ class Bot(object):
         pass
 
     def act(self, xdif, ydif, vel):
-      	if ydif<38:
-            return 1
+        if vel<-7:
+            if ydif<40:
+                return 1
+        if 4>vel>=-7:
+            if ydif<35:
+                return 1
