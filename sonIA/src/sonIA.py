b49765b9cd3f060d17b4f3ba1d62f3709c83102d
Retr0-viseur <remi.mergen@gmail.com>
2020-12-03 02:18:08 +0100

    Functional IA

--- sonIA/src/sonIA.py
+++ sonIA/src/sonIA.py
@@ -5,8 +5,8 @@ class Bot(object):
 
     def act(self, xdif, ydif, vel):
         if vel<-7:
-            if ydif<50:
+            if ydif<60:
                 return 1
         if vel>=-7:
-            if ydif<55:
+            if ydif<75:
                 return 1
